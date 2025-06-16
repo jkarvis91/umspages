2201df5858997aefd7fc3000370a2fb6430f6814
jkarvis91 <jkarvis91@gmail.com>
2025-06-16 17:29:50 +0900

    Add files via upload

---
+++ static/js/common/zoom.js
@@ -0,0 +1,3 @@
+    // set zoom event
+    $("#zoom-in-btn").off('click').on('click', function () { UmsZoom.zoomIn();});
+    $("#zoom-out-btn").off('click').on('click', function () { UmsZoom.zoomOut();});
